37b05008a18813661bb418cbff7610c04b4d0a6d
dev <domemacintosh@gmail.com>
2020-07-05 19:24:11 +0700

    Unsubscribe and complete in step 1

--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
+import {Observable, Subscription, interval} from 'rxjs';
+import {share} from 'rxjs/internal/operators';
+import { take, first } from 'rxjs/operators';
+ 
 console.clear();
 
 // See the README.MD for what code we will be writing in this section.
+const promise = new Promise((resolve, reject) => {
+  // some time goes
+  console.log('Creating the Promise')
+  resolve([1,2,3])
+  setTimeout(() => resolve([7,8,9]), 1000)
+})
+
+const observable = new Observable((observer) => {
+  // some time goes
+  console.log('Creating the Observable')
+  observer.next([4,5,6])
+  observer.complete()
+  //observer.error()
+  setTimeout(() => observer.next([7,8,9]), 1000)
+  setTimeout(() => observer.error('error!!!'), 2000)
+})
+
+/* promise.then(result => {
+  console.log(result)
+}, err => console.log(err))
+setTimeout( () => {
+  promise.then(result => {
+    console.log('second time', result)
+  })
+}, 4000) */
+
+/* observable.subscribe(result => {
+  console.log(result)
+},
+err => console.log('err', err)) */
+
+let result: Subscription = observable.subscribe(
+  val => console.log(val), // ? next callback
+  err => console.log(err), // ? error callback
+  () => console.log('Complete') // ? complete callback
+)
+// result.unsubscribe();
+
+// observable.subscribe(console.log)
+
+const o = new Observable(observer => {
+  console.log("HTTP")
+  setTimeout(() => {
+    console.log('Did HTTP')
+    observer.next(['x','y','z'])
+  },1000)
+
+// }).pipe(share())
+})
+
+o.subscribe(res => console.log('sub1',res))
+o.subscribe(res => console.log('sub2',res))
+o.subscribe(res => console.log('sub3',res))
+
+const myObs = o.pipe(share())
+myObs.subscribe(res => console.log('sub1-share',res))
+myObs.subscribe(res => console.log('sub2-share',res))
+myObs.subscribe(res => console.log('sub3-share',res))
+
+interval(1000)
+.pipe(
+  take(5)
+).subscribe(console.log, console.log, () => console.log('Complete'))
