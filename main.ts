ca265ddb27d897fdee15097cfe75eafc697b5cd6
ddotx <domemacintosh@gmail.com>
2020-11-24 13:08:32 +0700

    update

--- main.ts
+++ main.ts
@@ -1,95 +1,43 @@
-import { Observable, Subscription, interval } from 'rxjs';
-import { first, shareReplay, take } from 'rxjs/operators';
+import { share, shareReplay } from 'rxjs/operators'
 
-import { share } from 'rxjs/internal/operators';
+import { Observable } from 'rxjs'
 
-console.clear();
+console.clear()
 
-// See the README.MD for what code we will be writing in this section.
-// const promise = new Promise((resolve, reject) => {
-//   // some time goes
-//   console.log('Creating the Promise')
-//   resolve([1, 2, 3])
-//   setTimeout(() => resolve([7, 8, 9]), 1000)
-// })
+/**
+ * Multi-casting
+ */
+const o = new Observable((observer) => {
+  console.log('making call to server')
+  observer.next('THIS IS AN OBSERVABLE1')
+  observer.next('THIS IS AN OBSERVABLE2')
+}).pipe(
+  // shareReplay()
+  shareReplay({ refCount: true, bufferSize: 1 })
+)
 
-const observable = new Observable((observer) => {
-  // some time goes
-  console.log('Creating the Observable')
-  observer.next([4, 5, 6])
-  observer.complete()
-  //observer.error()
-  setTimeout(() => observer.next([7, 8, 9]), 1000)
-  setTimeout(() => observer.error('error!!!'), 2000)
+/* const s1 = o.subscribe(console.log)
+const s2 = o.subscribe(console.log)
+s1.unsubscribe()
+s2.unsubscribe()
+
+o.subscribe(console.log)
+ */
+
+const sourceObs = new Observable((observer) => {
+  console.log('making call to server')
+  observer.next('THIS IS AN OBSERVABLE1')
+  observer.next('THIS IS AN OBSERVABLE2')
 })
 
-/* promise.then(result => {
-  console.log(result)
-}, err => console.log(err))
-setTimeout( () => {
-  promise.then(result => {
-    console.log('second time', result)
-  })
-}, 4000) */
+const sharedObs = sourceObs.pipe(
+  // shareReplay(1)
+  share()
+)
 
-/* observable.subscribe(result => {
-  console.log(result)
-},
-err => console.log('err', err)) */
+const sub1 = sourceObs.subscribe(console.log)
+const sub2 = sourceObs.subscribe(console.log)
 
-// let result: Subscription = observable.subscribe(
-//   val => console.log(val), // ? next callback
-//   err => console.log(err), // ? error callback
-//   () => console.log('Complete') // ? complete callback
-// )
-// result.unsubscribe();
-
-// observable.subscribe(console.log)
-
-const o = new Observable(observer => {
-  console.log("HTTP")
-  setTimeout(() => {
-    console.log('Did HTTP')
-    observer.next(['x', 'y', 'z'])
-  }, 1000)
-
-  // }).pipe(share())
-})
-
-// o.subscribe(res => console.log('sub1', res))
-// o.subscribe(res => console.log('sub2', res))
-// o.subscribe(res => console.log('sub3', res))
-
-// const myObs = o.pipe(share())
-// myObs.subscribe(res => console.log('sub1-share', res))
-// myObs.subscribe(res => console.log('sub2-share', res))
-// myObs.subscribe(res => console.log('sub3-share', res))
-
-// interval(1000)
-// .pipe(
-//   take(5)
-// ).subscribe(console.log, console.log, () => console.log('Complete'))
-
-const o2 = new Observable(observer => {
-
-  observer.next(1)
-  observer.next(2)
-  observer.next(3)
-
-  setTimeout((() => observer.next(4)), 100)
-
-  setTimeout((() => observer.next(5)), 500)
-
-})
-  .pipe(share()
-    ,
-    shareReplay(1))
-//.pipe(shareReplay(1))
-
-o2.subscribe((res) => console.log('sub1', res))
-
-o2.subscribe((res) => console.log('sub2!!!!!!', res))
-
-o2.subscribe((res) => console.log('sub3-------------', res))
-
-o2.subscribe((res) => console.log('sub4%%%%-------------', res))
+const sub3 = sharedObs.subscribe((res) => console.log('1', res))
+const sub4 = sharedObs.subscribe((res) => console.log('2', res))
+const sub5 = sharedObs.subscribe((res) => console.log('3', res))
